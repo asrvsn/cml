f0595946b1510e3c36bb188ad5f2b3ca3d3babe2
Anand Srinivasan <asrini@mit.edu>
2020-12-02 11:34:47 -0500

    remove unstable matrix logarithm

--- logistic_2d_lyapunov.py
+++ logistic_2d_lyapunov.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.linalg import logm
 from numpy.linalg import eigvals
+import numpy as np
 import pdb
 
 from cml import *
@@ -16,32 +17,33 @@ class CML_logistic(CML_2D):
 		super().__init__(f, n, eps, u0=u0, fprime=fprime)
 
 n = 20
-T = 100
-t0 = 20
+T = 10
+t0 = 10
 
 ''' Lyapunov spectra of varying r ''' 
 
 eps = 0.5
-r_set = np.linspace(2.5, 3.9, 100)
+r_set = np.linspace(0, 4.0, 200)
 spectra = []
 for r in r_set:
 	print(f'r: {r}')
 	model = CML_logistic(n, r, eps=eps)
 	for _ in range(t0):
 		model.step()
-	term = np.eye(n*n)
+	term = np.zeros(n)
 	for _ in range(T):
 		jac = model.jacobian
-		term = jac@term@jac.T
-	term = logm(term) / (2*T)
-	spectra.append(eigvals(term))
-spectra = np.real(np.array(spectra))
+		Q, R = np.linalg.qr(jac, mode='complete')
+		term += np.log(np.abs(np.diag(R)[:n]))
+	term /= T
+	spectra.append(term)
+spectra = np.array(spectra)
 
 fig, ax = plt.subplots(figsize=(12, 5))
 for i in range(spectra.shape[1]):
-	ax.scatter(r_set, spectra[:,i], color='grey', s=1)
+	ax.scatter(r_set, spectra[:,i], color='blue', s=1)
 ax.plot(r_set, np.zeros_like(r_set), color='black')
-ax.set_xlabel('Logistic map $r$')
+ax.set_xlabel('Nonlinearity $r$')
 
 fig.tight_layout()
 fig.savefig('logistic_r_le.png', bbox_inches='tight', pad_inches=0)
@@ -49,25 +51,26 @@ plt.show()
 
 ''' Lyapunov spectra of varying eps ''' 
 
-r = 3.5
-eps_set = np.linspace(0, 1., 100)
+r = 3.2
+eps_set = np.linspace(0, 1., 200)
 spectra = []
 for eps in eps_set:
 	print(f'eps: {eps}')
 	model = CML_logistic(n, r, eps=eps)
 	for _ in range(t0):
 		model.step()
-	term = np.eye(n*n)
+	term = np.zeros(n)
 	for _ in range(T):
 		jac = model.jacobian
-		term = jac@term@jac.T
-	term = logm(term) / (2*T)
-	spectra.append(eigvals(term))
-spectra = np.real(np.array(spectra))
+		Q, R = np.linalg.qr(jac, mode='complete')
+		term += np.log(np.abs(np.diag(R)[:n]))
+	term /= T
+	spectra.append(term)
+spectra = np.array(spectra)
 
 fig, ax = plt.subplots(figsize=(12, 5))
 for i in range(spectra.shape[1]):
-	ax.scatter(eps_set, spectra[:,i], color='grey', s=1)
+	ax.scatter(eps_set, spectra[:,i], color='blue', s=1)
 ax.plot(eps_set, np.zeros_like(eps_set), color='black')
 ax.set_xlabel('Coupling constant $\\varepsilon$')
 
