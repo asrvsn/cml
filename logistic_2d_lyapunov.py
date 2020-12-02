4cf62540867d8a036c774600af57199e5465d915
Anand Srinivasan <asrini@mit.edu>
2020-12-02 00:50:38 -0500

    parameters

--- logistic_2d_lyapunov.py
+++ logistic_2d_lyapunov.py
@@ -17,14 +17,15 @@ class CML_logistic(CML_2D):
 
 n = 20
 T = 100
-t0 = 10
+t0 = 20
 
 ''' Lyapunov spectra of varying r ''' 
 
 eps = 0.5
-r_set = np.linspace(2.9, 4, 100)
+r_set = np.linspace(2.5, 3.9, 100)
 spectra = []
 for r in r_set:
+	print(f'r: {r}')
 	model = CML_logistic(n, r, eps=eps)
 	for _ in range(t0):
 		model.step()
@@ -35,12 +36,12 @@ for r in r_set:
 	term = logm(term) / (2*T)
 	spectra.append(eigvals(term))
 spectra = np.real(np.array(spectra))
-# pdb.set_trace()
 
 fig, ax = plt.subplots(figsize=(12, 5))
-ax.plot(r_set, np.zeros_like(r_set), color='black')
 for i in range(spectra.shape[1]):
-	ax.plot(r_set, spectra[:,i], color='grey')
+	ax.scatter(r_set, spectra[:,i], color='grey', s=1)
+ax.plot(r_set, np.zeros_like(r_set), color='black')
+ax.set_xlabel('Logistic map $r$')
 
 fig.tight_layout()
 fig.savefig('logistic_r_le.png', bbox_inches='tight', pad_inches=0)
@@ -48,23 +49,28 @@ plt.show()
 
 ''' Lyapunov spectra of varying eps ''' 
 
-# r = 3.9
-# eps_set = [0.0001]
-# spectra = []
-# for eps in eps_set:
-# 	model = CML_logistic(n, r, eps=eps)
-# 	for _ in range(t0):
-# 		model.step()
-# 	term = np.eye(n*n)
-# 	for _ in range(T):
-# 		jac = model.jacobian
-# 		term = jac@term@jac.T
-# 	term = logm(term) / (2*T)
-# 	spectra.append(eigvals(term))
+r = 3.5
+eps_set = np.linspace(0, 1., 100)
+spectra = []
+for eps in eps_set:
+	print(f'eps: {eps}')
+	model = CML_logistic(n, r, eps=eps)
+	for _ in range(t0):
+		model.step()
+	term = np.eye(n*n)
+	for _ in range(T):
+		jac = model.jacobian
+		term = jac@term@jac.T
+	term = logm(term) / (2*T)
+	spectra.append(eigvals(term))
+spectra = np.real(np.array(spectra))
 
-# fig, ax = plt.subplots(figsize=(12, 5))
-# ax.scatter(spectra, r_set)
+fig, ax = plt.subplots(figsize=(12, 5))
+for i in range(spectra.shape[1]):
+	ax.scatter(eps_set, spectra[:,i], color='grey', s=1)
+ax.plot(eps_set, np.zeros_like(eps_set), color='black')
+ax.set_xlabel('Coupling constant $\\varepsilon$')
 
-# fig.tight_layout()
-# fig.savefig('logistic_eps_le.png', bbox_inches='tight', pad_inches=0)
-# plt.show()
+fig.tight_layout()
+fig.savefig('logistic_eps_le.png', bbox_inches='tight', pad_inches=0)
+plt.show()
