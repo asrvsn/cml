ced6fe5c5791064f5fd0e6409a020792618ee7aa
Anand Srinivasan <asrini@mit.edu>
2020-12-06 23:58:50 -0500

    supplementary

--- bubbles.py
+++ bubbles.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
 	t0 = 1000
 	tn = 100
 	k = 3
-	temp_rng = np.linspace(0, 11, 2)
+	temp_rng = np.linspace(9.7, 10.05, 100)
 	data = []
 	for temp in tqdm(temp_rng):
 		model = CML_bubbles((m, n), temp, eps, sigma, alpha, eta, T_c)
@@ -146,11 +146,61 @@ if __name__ == '__main__':
 
 	fig, ax = plt.subplots(figsize=(12, 4))
 	ax.plot(temp_rng, data)
+	ax.set_xlabel('External temperature $E$')
 
 	fig.tight_layout()
 	fig.savefig('bubbles_curve.png', bbox_inches='tight', pad_inches=0)
 
 	plt.show()
+
+	''' Lyapunov spectra ''' 
+	# t0 = 1000
+	# tn = 20
+	# temp_rng = np.linspace(9.8, 10.1, 75)
+	# n_perturb = 40
+	# eps_perturb = 0.5
+	# k_exponents = m*n
+
+	# spectra = []
+	# for temp in tqdm(temp_rng):
+	# 	model = CML_bubbles((m, n), temp, eps, sigma, alpha, eta, T_c)
+
+	# 	for _ in range(t0):
+	# 		model.step()
+
+	# 	perturbed = []
+	# 	for _ in range(n_perturb):
+	# 		model_ = CML_bubbles((m, n), temp, eps, sigma, alpha, eta, T_c)
+	# 		model_.T = model.T.copy() 
+	# 		model_.T[1:-1] += np.random.normal(size=(m-2, n), scale=eps_perturb)
+	# 		perturbed.append(model_)
+
+	# 	spectrum = np.zeros(k_exponents)
+	# 	ds = np.vstack([(model_.T - model.T).ravel() for model_ in perturbed])
+	# 	for _ in range(tn):
+	# 		for model_ in perturbed:
+	# 			model_.step()
+	# 		model.step()
+	# 		ds_ = np.vstack([(model_.T - model.T).ravel() for model_ in perturbed])
+	# 		jac = np.linalg.lstsq(ds, ds_)[0]
+	# 		Q, R = np.linalg.qr(jac.T, mode='complete')
+	# 		spectrum += np.log(np.abs(np.diag(R)[:k_exponents]))
+	# 		ds = ds_
+	# 	spectra.append(spectrum)
+	# spectra = np.array(spectra)
+
+	# fig, ax = plt.subplots(figsize=(12, 5))
+	# for i in range(spectra.shape[1]):
+	# 	ax.scatter(temp_rng, spectra[:,i], color='blue', s=1)
+	# envelope = spectra.max(axis=1)
+	# ax.plot(temp_rng, envelope, color='black', alpha=0.5)
+	# ax.plot(temp_rng, np.zeros_like(temp_rng), color='black')
+	# ax.set_ylim(ymin=-100)
+	# ax.set_xlabel('External temperature $E$')
+
+	# fig.tight_layout()
+	# fig.savefig('bubbles_lyapunov.png', bbox_inches='tight', pad_inches=0)
+	# plt.show()
 
 	''' Save as video ''' 
 	# from matplotlib.animation import FFMpegWriter
--- rb_bubbles.py
+++ rb_bubbles.py
@@ -15,7 +15,7 @@ np.random.seed(9999)
 class CML_boiling:
 	def __init__(self, shape: Tuple, 
 			temp: float, nu: float, eta1: float, kappa: float, c: float, 
-			eps: float, sigma: float, alpha: float, eta2: float, T_c: float,
+			sigma: float, alpha: float, eta2: float, T_c: float,
 			dt: float=1.0
 		):
 		'''
@@ -35,7 +35,6 @@ class CML_boiling:
 		self.kappa = kappa
 		self.c = c
 
-		self.eps = eps
 		self.sigma = sigma
 		self.alpha = alpha
 		self.eta2 = eta2
@@ -90,7 +89,7 @@ class CML_boiling:
 		return ret
 
 	def density(self, T):
-		return np.tanh(self.alpha*(T+self.temp-self.T_c))
+		return np.tanh(self.alpha*(T-self.T_c))
 
 	def step(self):
 		# Euler step
@@ -107,28 +106,30 @@ class CML_boiling:
 		for y in range (1, m-1):
 			for x in range(n):
 				mult = 0.
-				if T[y, x]+self.temp > self.T_c and self.T[y, x]+self.temp < self.T_c:
+				if T[y, x] > self.T_c and self.T[y, x] < self.T_c:
 					mult = -1.
-				elif T[y, x]+self.temp < self.T_c and self.T[y, x]+self.temp > self.T_c:
+				elif T[y, x] < self.T_c and self.T[y, x] > self.T_c:
 					mult = 1.
 				T[y, (x+1)%m] += mult*self.eta2
 				T[y, (x-1)%m] += mult*self.eta2
 				T[y-1, x] += mult*self.eta2
 				T[y+1, x] += mult*self.eta2
 
+		self.T = T
+
 		# Lagrange step
-		self.T = self.advect(T)
+		self.T = self.advect(self.T)
 		self.Vy = self.advect(self.Vy)
 		self.Vx = self.advect(self.Vx)
 
 		# Boundary conditions
-		self.T[0,:] = -self.temp
+		self.T[0,:] = 0.
 		self.T[-1,:] = self.temp
 
 	def reset(self):
 		# Symmetry-breaking initial conditions
-		self.T = np.random.normal(size=self.shape, scale=1e-3)
-		self.T[0,:] = -self.temp
+		self.T = self.temp / 2 + np.random.normal(size=self.shape, scale=1e-3)
+		self.T[0,:] = 0.
 		self.T[-1,:] = self.temp
 		self.Vx = np.random.normal(scale=1e-3, size=self.shape)
 		self.Vy = np.random.normal(scale=1e-3, size=self.shape)
@@ -136,58 +137,58 @@ class CML_boiling:
 if __name__ == '__main__':
 	m, n = 25, 50
 
-	temp = 9.87/2
+	temp = 9.95
 
-	c = 1.0
-	nu = 0.5
+	nu = 0.1
 	eta1 = 0.3
 	kappa = 0.8
+	c = 1.0
 
-	eps = 0.5
 	sigma = 0.1
-	alpha = 10
+	alpha = 5
 	eta2 = 0.1
 	T_c = 10
 
-	model = CML_boiling((m, n), temp, nu, eta1, kappa, c, eps, sigma, alpha, eta2, T_c)
+	model = CML_boiling((m, n), temp, nu, eta1, kappa, c, sigma, alpha, eta2, T_c, dt=1.0)
 
 	# pdb.set_trace()
 
 	''' Save as plot ''' 
 
-	# for _ in range(1000):
-	# 	model.step()
+	for _ in range(900):
+		model.step()
 
-	# fig, ax = plt.subplots(figsize=(12, 6))
-	# ax.contourf(model.T, 20, cmap='inferno')
-	# ax.invert_yaxis()
-	# ax.axis('off')
+	fig, ax = plt.subplots(figsize=(12, 6))
+	ax.contourf(model.T, 20, cmap='inferno')
+	ax.invert_yaxis()
+	# sns.heatmap(model.T, ax=ax)
+	ax.axis('off')
 
-	# fig.tight_layout()
-	# fig.savefig('rb_bubbles.png', bbox_inches='tight', pad_inches=0)
+	fig.tight_layout()
+	fig.savefig('rb_bubbles.png', bbox_inches='tight', pad_inches=0)
 
-	# fig, ax = plt.subplots(figsize=(12, 6))
-	# ax.quiver(model.Vx, model.Vy)
-	# ax.axis('off')
+	fig, ax = plt.subplots(figsize=(12, 6))
+	ax.quiver(model.Vx, model.Vy)
+	ax.axis('off')
 
-	# fig.tight_layout()
-	# fig.savefig('rb_convection_field.png', bbox_inches='tight', pad_inches=0)
+	fig.tight_layout()
+	fig.savefig('rb_bubbles_field.png', bbox_inches='tight', pad_inches=0)
 
-	# plt.show()
+	plt.show()
 
 	''' Save as video ''' 
-	from matplotlib.animation import FFMpegWriter
+	# from matplotlib.animation import FFMpegWriter
 
-	writer = FFMpegWriter(fps=15, metadata={'title': 'rb_bubbles'})
-	fig, axs = plt.subplots(1, 2, figsize=(13, 4))
+	# writer = FFMpegWriter(fps=15, metadata={'title': 'rb_bubbles'})
+	# fig, axs = plt.subplots(1, 2, figsize=(13, 4))
 
-	with writer.saving(fig, 'rb_bubbles.mp4', dpi=100):
-		model.reset()
-		for _ in tqdm(range(1000)):
-			axs[0].clear()
-			axs[1].clear()
-			axs[0].contourf(model.T, 20, cmap='inferno')
-			axs[0].invert_yaxis()
-			axs[1].quiver(model.Vx, model.Vy)
-			writer.grab_frame()
-			model.step()
+	# with writer.saving(fig, 'rb_bubbles.mp4', dpi=100):
+	# 	model.reset()
+	# 	for _ in tqdm(range(1000)):
+	# 		axs[0].clear()
+	# 		axs[1].clear()
+	# 		axs[0].contourf(model.T, 20, cmap='inferno')
+	# 		axs[0].invert_yaxis()
+	# 		axs[1].quiver(model.Vx, model.Vy)
+	# 		writer.grab_frame()
+	# 		model.step()
